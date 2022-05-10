bc2593e23455da6ad940dc3988ffadf6450c735a
Dr. Stefan Schade <dr_stefan_schade@yahoo.com>
2022-05-10 18:27:56 +0200

    add ssh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Browser refreshes automatically
+          Edit <code>src/App.js</code> and save to reload. Testing refresh
         </p>
         <a
           className="App-link"
